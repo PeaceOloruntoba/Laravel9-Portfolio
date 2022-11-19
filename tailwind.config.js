c924c2f28e7d87f09734ebd8e280423cdd022739
PeaceOloruntoba <profprincepeace@gmail.com>
2022-11-19 01:25:43 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("./plugin")],
+}
